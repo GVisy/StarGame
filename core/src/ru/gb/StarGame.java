7a675acf2b634e3835211df790d4f50d51299ab6
vglibin <vglibin@mail.ru>
2021-08-02 20:40:59 +0700

    Initial commit

---
+++ core/src/ru/gb/StarGame.java
@@ -0,0 +1,34 @@
+package ru.gb;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import org.graalvm.compiler.core.target.Backend;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0.4f, 0.24f, 051f, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
